e9c92cd8046454dc8c98ef4a07761be021c07dab
David Desborough-Smith <100168578+Shortbottom@users.noreply.github.com>
2024-03-21 10:55:38 +0000

    Change to new GUI

--- Core.lua
+++ Core.lua
@@ -2,91 +2,147 @@ DLTAddOn = LibStub("AceAddon-3.0"):NewAddon("DLTAddOn", "AceConsole-3.0")
 local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
-local AceGUI = LibStub("AceGUI-3.0")
-
+local AceDBOpt = LibStub("AceDBOptions-3.0")
 local appName = "Dungeon Loot Tracker"
-local windowShown = false
+local parentFrame = DLT_Parent_
+
+local instanceTable = {
+    InstanceType = "Dungeon",
+    Difficulties = {
+
+    },
+    difficulty = "Normal",
+
+}
 
 local defaults = {
     profile = {
-        toggleOption = true,
-        playerName = "",
-        msg = "",
-        subOptions = {
-            subOptA = false,
-            subOptB = true
-        }
+        autoRecord = false, -- We don't want the AddOn to automatically do something unless the User has chosen for it to
     }
 }
 
-local DLTOptions = {
+-- KEEP FOR POSSIBLE USE LATER
+-- Journalator.Archiving.LoadUpTo(earliestTimestamp, function()
+--     local lootContainers = CopyTable(Journalator.Archiving.GetRange(earliestTimestamp, "LootContainers")
+-- end)
+
+local mainSettingsInfo = {
     name = appName,
     handler = DLTAddOn,
     type = "group",
     args = {
-        toggleOption = {
-            name = "Toggle Option",
-            desc = "Toggle Option On/Off",
-            type = "toggle",
-            set = "SetToggleOption",
-            get = function(info) return DLTAddOn.db.profile.toggleOption end,
-        },
-        msg = {
-            name = "Welcome Message",
-            desc = "The message to welcome you to the game",
-            type = "input",
-            set = "SetMyMessage",
-            get = "GetMyMessage"
+        info = {
+            type = "header",
+            name = "",
+            desc = "Name of the Add On"
         }
     }
 }
 
-AceConfig:RegisterOptionsTable(appName .. " Options", DLTOptions)
-AceConfigRegistry:RegisterOptionsTable(appName .. " Options", DLTOptions, true)
+local generalOptions = {
+    name = appName .. ": General",
+    handler = DLTAddOn,
+    type = "group",
+    args = {
+        info = {
+            type = "header",
+            name = "",
+            desc = ""
+        },
+        autoRecord = {
+            type = "toggle",
+            name = "Auto Record",
+            desc = "Do you want to start recording as soon as you enter an instance without being asked?",
+            get = "getAutoRecord",
+            set = "setAutoRecord",
+        }
+    }
+}
+
+-- AceConfigRegistry:RegisterOptionsTable(appName .. " Options", options, true)
 
 function DLTAddOn:OnInitialize()
-    -- Code that runs when first loaded - i.e restore settings
-    DLTAddOn:Print(appName, ": OnInitialize")
-    self.optionsFrame = AceConfigDialog:AddToBlizOptions(appName .. " Options", appName, nil)
-    self.db = LibStub("AceDB-3.0"):New("dltDB", defaults)
-    self.db.profile.playerName = GetUnitName("player")
-    self:RegisterChatCommand("dlt", "toggleWindow")
+    -- Load the SavedVariables
+    self.db = LibStub("AceDB-3.0"):New("dltDB", defaults, profile)
+    self.db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
+    self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
+    self.db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
 
-    DLTAddOn:createMainWindow()
+    ------------------ Setup the options and add it to the Blizzard AddOn options frame ------------------
+
+    -- First Page should be an info page
+    AceConfig:RegisterOptionsTable("DungeonLootTracker_options", mainSettingsInfo)
+    self.optionsFrame = AceConfigDialog:AddToBlizOptions("DungeonLootTracker_options", appName)
+
+    -- Add the General Tab
+    AceConfig:RegisterOptionsTable("DungeonLootTracker_General", generalOptions)
+    AceConfigDialog:AddToBlizOptions("DungeonLootTracker_General", "General", appName)
+
+    -- Add the use of profiles
+    local profiles = AceDBOpt:GetOptionsTable(self.db)
+    AceConfig:RegisterOptionsTable("DungeonLootTracker_Profiles", profiles)
+    AceConfigDialog:AddToBlizOptions("DungeonLootTracker_Profiles", "Profiles", appName)
+
+    -- Register Chat Commands
+    self:RegisterChatCommand("dlt", "slashCommand")
+    -- self:RegisterChatCommand("dungeonloottracker", "slashCommand")
+
+    -- INFO: Doesn't really matter but set the default to be not recording;
+    -- INFO: This is just to ensure that the key is set to a value and isn't left as null
+    DLT_Parent_ToggleRecordingBtn.recording = false
 end
 
 function DLTAddOn:OnEnable()
     -- Code that runs when the AddOn is enabled
-    DLTAddOn:Print("Enabling AddOn")
     DLTAddOn:Enable()
 end
 
 function DLTAddOn:OnDisable()
     -- Code that runs when the AddOn is disabled
-    DLTAddOn:Print("Disabling AddOn")
     DLTAddOn:Disable()
 end
 
-function DLTAddOn:GetMyMessage(info)
-    return self.db.profile.msg
-end
-
-function DLTAddOn:SetMyMessage(info, input)
-    self.db.profile.msg = input
-    DLTAddOn:Print(self.db.profile.msg)
+function DLTAddOn:slashCommand(msg)
+    if not msg or msg:trim() == "" then
+        -- If no args are passed in the slash command we just want to toggle the window being shown
+        if DLT_Parent_:IsShown() == true then
+            DLT_Parent_:Hide()
+        else
+            DLT_Parent_:Show()
+        end
+    elseif msg == "options" then
+        -- If options are passed as an arg in the slash command we want to option the blizzard options panel
+        InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+        InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+    elseif msg == "help" then
+        local helpMsg = "Dungeon Loot Tracker Commands"
+        helpMsg = helpMsg .. "\n /dlt -- will toggle the window being shown."
+        helpMsg = helpMsg .. "\n /dlt options -- opens the AddOn options in the blizzard options window"
+        helpMsg = helpMsg .. "\n /dlt help -- shows this help message."
+        self:Print(helpMsg)
+    else
+        self:Print(msg)
+    end
 end
 
 -- Option Functions
-
-function DLTAddOn:SetToggleOption(info, val)
-    self.db.profile.toggleOption = val
+function DLTAddOn:RefreshConfig()
     AceConfigRegistry:NotifyChange(appName)
+end
+
+function DLTAddOn:setAutoRecord(info, value)
+    self.db.profile.autoRecord = value
+    AceConfigRegistry:NotifyChange(appName)
+end
+
+function DLTAddOn:getAutoRecord(info)
+    return self.db.profile.autoRecord
 end
 
 function DLTAddOn:openOptions(input)
     InterfaceOptionsFrame_OpenToCategory(appName .. " Options")
 end
-`
+
 function DLTAddOn:toggleWindow(input)
     if windowShown then
         AceConfigDialog:Close(appName .. " Options")
@@ -95,26 +151,57 @@ function DLTAddOn:toggleWindow(input)
     end
 end
 
--- Create the GUI Windows
+function DLTAddOn:showFrame()
+    DLT_MainWindow:Show()
+end
 
-function DLTAddOn:createMainWindow()
-    -- Create the Main Window Frame
-    local f = AceGUI:Create("Window")
-    f:SetCallback("OnClose", function(widget)
-        windowShown = false
-        AceGUI:Release(widget)
-    end)
-    f:SetTitle(appName)
-    f:SetStatusText("Status Bar")
-    f:SetLayout("Flow")
-    f:SetWidth(150)
+function DLTAddOn:HideTooltip()
+    print("Hiding Tooltip")
+end
 
-    -- Create a button
-    local btn = AceGUI:Create("Button")
-    btn:SetWidth(100)
-    btn:SetText("Button")
-    btn:SetCallback("OnClick", function() print("You clicked me!") end)
-    f:AddChild(btn)
+function DLTAddOn:ResetAllInstances()
+    print("Ressetting all instances")
+end
 
-    windowShown = true
+function DLTAddOn:ChooseDifficulty()
+    local isShown = DLT_Difficulties:IsShown()
+    if isShown then
+        print("Hiding difficulty")
+        DLT_Difficulties:Hide()
+    else
+        print("Showing difficulty")
+        DLT_Difficulties:Show()
+    end
+end
+
+-- Functions to deal with starting and stopping recordings
+function DLTAddOn:ToggleRecording_OnClick(self)
+    print(self.recording)
+    if (self.recording) then
+        DLTAddOn:Record_Stop();
+        PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF);
+    else
+        DLTAddOn:Record_Start();
+        PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+    end
+end
+
+function DLTAddOn:IsRecording()
+    return DLT_Parent_ToggleRecordingBtn.recording;
+end
+
+function DLTAddOn:Record_Start()
+    if (DLT_Parent_ToggleRecordingBtn.recording) then
+        return;
+    end
+    DLT_Parent_ToggleRecordingBtn.recording = true;
+    DLT_Parent_ToggleRecordingBtn:SetNormalTexture("Interface\\TimeManager\\ResetButton");
+end
+
+function DLTAddOn:Record_Stop()
+    if (not DLT_Parent_ToggleRecordingBtn.recording) then
+        return;
+    end
+    DLT_Parent_ToggleRecordingBtn.recording = false;
+    DLT_Parent_ToggleRecordingBtn:SetNormalTexture("Interface\\Buttons\\UI-SpellbookIcon-NextPage-Up");
 end
