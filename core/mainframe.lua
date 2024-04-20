a9d6e246e814c09734454b18e6996bc92f66b951
Shortbottom <shorttbottom@gmail.com>
2024-04-20 11:16:30 +0100

    Refactor mainframe.lua to update toggleFrame function and add Addon class

--- core/mainframe.lua
+++ core/mainframe.lua
@@ -1,13 +1,12 @@
 local addonName = ... ---@type string
 
----@class DLT_Addon: AceAddon
+---@class Addon
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 
 ---@class MainFrame: AceModule
 local mainFrame = addon:NewModule("MainFrame")
 
 ---Show/Hide a frame
----@param name string # Name of a frame as a string
 function mainFrame:toggleFrame()
   local f = _G["DLT_Parent_"]
   if f == nil then
