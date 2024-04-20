a32d6ec8bc2d18f5e51b2cc8c0283ff83bf0158f
Shortbottom <shorttbottom@gmail.com>
2024-04-20 11:16:19 +0100

    Refactor database.lua to update class definitions and add Addon class

--- core/database.lua
+++ core/database.lua
@@ -1,13 +1,12 @@
 local addonName = ... ---@type string
 
----@class DLT_Addon: AceAddon
+---@class Addon
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 
 ---@class Constants: AceModule
 local constants = addon:GetModule("Constants")
 
 ---@class Database: AceModule
----@field private data databaseOptions
 local DB = addon:NewModule("Database")
 
 function DB:OnInitialize()
