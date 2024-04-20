b16b00fe9c6926a800e2ed7d727a94a844107a99
Shortbottom <shorttbottom@gmail.com>
2024-04-20 11:16:36 +0100

    Refactor metadata.lua to update GetAddOnMetadata function and add Addon class

--- core/metadata.lua
+++ core/metadata.lua
@@ -1,24 +1,24 @@
 local addonName = ... ---@type string
 
----@class DLT_Addon: AceAddon
+---@class Addon: AceAddon
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 
 ---@class Metadata: AceModule
 local metadata = addon:NewModule("Metadata")
 
 ---Get all the Metadata and store it locally in our Addon Namespace
----@param addonName string The Addon name as wow sees it
+---@param name string The Addon name as wow sees it
 ---@return table Return a table containing all the Metadata
-function metadata:GetAddOnMetadata(addonName)
-  local version = C_AddOns.GetAddOnMetadata(addonName, "Version")
-  local title = C_AddOns.GetAddOnMetadata(addonName, "Title")       --addonName -- Name of the AddOn folder
-  local acronym = C_AddOns.GetAddOnMetadata(addonName, "X-Acronym") -- Short abbreviation for the addonName
-  local notes = C_AddOns.GetAddOnMetadata(addonName, "Notes")
-  local icon = C_AddOns.GetAddOnMetadata(addonName, "IconTexture")  --"Interface\\Addons\\DungeonLootTracker\\Images\\icon"
-  local author = C_AddOns.GetAddOnMetadata(addonName, "Author")
-  local wago = C_AddOns.GetAddOnMetadata(addonName, "X-Wago-ID")
-  local curse = C_AddOns.GetAddOnMetadata(addonName, "X-Curse-Project-ID")
-
+function metadata:GetAddOnMetadata(name)
+  local version = C_AddOns.GetAddOnMetadata(name, "Version")
+  local title = C_AddOns.GetAddOnMetadata(name, "Title")       --name -- Name of the AddOn folder
+  local acronym = C_AddOns.GetAddOnMetadata(name, "X-Acronym") -- Short abbreviation for the addonName
+  local notes = C_AddOns.GetAddOnMetadata(name, "Notes")
+  local icon = C_AddOns.GetAddOnMetadata(name, "IconTexture")  --"Interface\\Addons\\DungeonLootTracker\\Images\\icon"
+  local author = C_AddOns.GetAddOnMetadata(name, "Author")
+  local wago = C_AddOns.GetAddOnMetadata(name, "X-Wago-ID")
+  local curse = C_AddOns.GetAddOnMetadata(name, "X-Curse-Project-ID")
+  local build = ""
   return {
     AddonName = addonName,
     Title = title,
