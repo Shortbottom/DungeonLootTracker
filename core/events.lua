71709b4b4525898b523f244607c3d54b9b4046ff
Shortbottom <shorttbottom@gmail.com>
2024-05-04 21:40:24 +0100

    Refactor events.lua: Update event registration and add recordingEvents table

--- core/events.lua
+++ core/events.lua
@@ -12,13 +12,15 @@ local db = addon:GetModule("Database")
 ---@class Events: AceEvent
 local events = addon:NewModule("Events")
 
+LibStub("AceEvent-3.0"):Embed(events)
 
+-- TODO: Move this to the recording area
 local _autoRecordEvents = {
   ["PLAYER_ENTERING_WORLD"] = "EnterWorld", -- This is the event we want to watch if autoRecord is true
   ["PLAYER_LEAVING_WORLD"] = "LeaveWorld"   -- This might be the better event to stop recording when autoRecord is true
 }
 
-local _recordingEvents = {
+local recordingEvents = {
   ["LOOT_READY"] = function () addon:SendMessage("recording/lootReady") end,
   ["LOOT_OPENED"] = function () addon:SendMessage("recording/lootOpened") end,
   ["LOOT_SLOT_CHANGED"] =
@@ -32,19 +34,14 @@ local _recordingEvents = {
   ["LOOT_SLOT_CLEARED"] =
   ---Sends a message indicating that a loot slot has been cleared.
   ---@param _ ignored - The first parameter (ignored).
-  ---@param lootSlot number - The loot slot that has been cleared.
-      function (_, lootSlot)
-        addon:SendMessage("recording/lootSlotCleared", lootSlot)
+  ---@param slot number - The loot slot that has been cleared.
+      function (_, slot)
+        addon:SendMessage("recording/lootSlotCleared", slot)
       end
 }
 
-LibStub("AceEvent-3.0"):Embed(events)
-
 function events:OnInitialize()
-  -- Register events to listen for
-  -- for k, v in pairs(_autoRecordEvents) do
-  --   events:RegisterEvent(k, v)
-  -- end
+
 end
 
 function events:EnterWorld(_, initialLogin, reload, ...)
@@ -73,14 +70,14 @@ end
 
 function events:startRecording_Events()
   --addon:Print("Listening to events")
-  for k, v in pairs(_recordingEvents) do
+  for k, v in pairs(recordingEvents) do
     events:RegisterEvent(k, v)
   end
 end
 
 function events:stopRecording_Events()
   --addon:Print("Stopping listening to events")
-  for k, v in pairs(_recordingEvents) do
+  for k, v in pairs(recordingEvents) do
     events:UnregisterEvent(k, v)
   end
 end
