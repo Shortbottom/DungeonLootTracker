38556f3e4254c014635b1f7a91a918253083081f
Shortbottom <shorttbottom@gmail.com>
2024-05-04 21:40:17 +0100

    Refactor boot.lua: Add AceEvent-3.0 module to the addon

--- core/boot.lua
+++ core/boot.lua
@@ -1,6 +1,6 @@
 local addonName, root = ... --[[@type string, table]]
 
 ---@class DLT_Addon: AceModule
-local addon = LibStub("AceAddon-3.0"):NewAddon(root, addonName, "AceConsole-3.0", "AceHook-3.0")
+local addon = LibStub("AceAddon-3.0"):NewAddon(root, addonName, "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0")
 
 addon:SetDefaultModuleState(false)
