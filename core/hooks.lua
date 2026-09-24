3bb4d34a83f403076c590ca2f28bc2c2ea690e4c
Shortbottom <shorttbottom@gmail.com>
2026-09-24 17:04:13 +0000

    Back up addon and uncommitted work before rebuild

--- core/hooks.lua
+++
@@ -1,7 +0,0 @@
-local addonName = ... ---@type string
-
----@class DLT_Addon: AceAddon
-local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
-
----@class Hooks: AceModule
-local hooks = addon:NewModule("Hooks")
